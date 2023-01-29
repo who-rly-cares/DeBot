8b8c503ce1f3545d87e67f2efbbba8e2ae6b689c
who-rly-cares <88319573+who-rly-cares@users.noreply.github.com>
2023-01-29 15:58:57 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+
+
+def f()->None:
+  #i just dont like main.py to be empty
+  #so u can enjoy calling a function till ethernity
+  globals.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__.__call__()
