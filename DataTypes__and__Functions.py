b7e88b95ecab24d4fcae11bb0c3e3394cfb8de56
who-rly-cares <88319573+who-rly-cares@users.noreply.github.com>
2023-01-31 23:52:03 +0330

    .

--- DataTypes__and__Functions.py
+++ DataTypes__and__Functions.py
@@ -2,53 +2,73 @@
 put functions and classes here
 #turn off word wrapp!!!
 """
+import json
+
 
 def PyDict__to__Json(a:dict):
-    return a.__str__().reaplce("\'","\"");
+    return json.dumps(a, indent=4);
 
 
 #----------------------------------------------------------------------
 class Professor:
-    def __init__(self, id:int, fullname:str, majors_teaching:list):
+    def __init__(self, id:int, fullname:str, fullname_farsi:str, majors_teaching:list):
         self.id = id
         self.fullname = fullname
+        self.fullname_farsi = fullname_farsi
         self.majors_teaching = majors_teaching
+
+
     def __hash__(self):
         return hash(self.title)
 
-    def return_as_json(self)->str:
-        pass 
-#----------------------------------------------------------------------
+
+
+    def return_as_dict(self)->str:
+        out = {
+        "id":self.id,
+        "fullname":self.fullname,
+        "fullname_farsi":self.fullname_farsi,
+        "majors_teaching":self.majors_teaching,
+        }
+        return out
+#================================================================================
 class ProfessorsTag:
-    def __init__(self, title:str, point:int, count:int, owner:Professor):
-        self.title = title
+    def __init__(self, title:str, farsi_title:str, point:int, count:int, owner:Professor):
+        self.title =title
         self.point = point
         self.count = count
+        self.farsi_title = farsi_title
+        self.owner = owner
+
 
     def __hash__(self):
         return hash(self.title)
 
 
-    def __setattr__(self, attrname, value):
-        ReadOnly = ["title", "point"]
-        if attrname in ReadOnly:
-            return None
-        else:
-            try:
-                return self.__dict__["attrname"]
-            except Exception as er:
-                return er
+
 
     @classmethod
     def sub_class_decorator(cls, subclass):
         pass
 
-    def return_as_json(self)->str:
-        pass 
-        
-        
-#----------------------------------------------------------------------
-class ProfessorRatingData:
+    def return_as_dict(self)->str:
+        out = {
+        "title":self.title,
+        "point":str(self.point),
+        "count":str(self.count),
+        "farsi_title":str(self.farsi_title)
+        }
+        return out
+#===============ProfessorsTag Subclasses
+class TestingProfessorTag(ProfessorsTag):
+    def __init__(self, owner, count):
+        title="A TESTING TAG"
+        farsi_title="فارسی"
+        point=+1
+        ProfessorsTag.__init__(self, title, farsi_title, point, count, owner)
+
+#================================================================================
+class ProfessorProfile:
     def __init__(self, owner:Professor, tags:list):
         self.owner = owner
         self.tags = tags
@@ -57,21 +77,39 @@ class ProfessorRatingData:
     def return_ProfessorRatingData_instance_by_json(self):
         "pass"
 
+    def return_tags_as_json(self):
+        out = []
+        for i1 in self.tags:
+            out.append(i1.return_as_dict())
+        return out.__str__()
+
     def return_as_json(self)->str:
         output = {
-        "owner":self.owner.return_as_json(),
+        "owner":self.owner.return_as_dict(),
         "tags":self.return_tags_as_json(),
         }
-        return str
+        return PyDict__to__Json(output)
 #----------------------------------------------------------------------
 #DATA BASE RELATED
-def write_ProfessorRatingData_to_db(a:ProfessorRatingData):
+def write_ProfessorProfile_to_db(a:ProfessorProfile):
     """
     this function accepts an instance of ProfessorRatingData
     and writes it in a standard form to the db
     """
-def return_ProfessorRatingData_from_db_by_professor_id(id):
+def return_ProfessorProfile_from_db_by_professor_id(id)->ProfessorProfile:
     """
     this function will return an isntance of ProfessorRatingData from data base
     put data base related code here
     """
+
+
+
+def GenralTest():
+    test_professor = Professor(id=0, fullname="Reza", fullname_farsi="رضا", majors_teaching=["math"])
+    test_profile = ProfessorProfile(owner=test_professor, tags=[TestingProfessorTag(test_professor, 0)])
+    print(test_profile.return_as_json())
+
+
+
+
+GenralTest()
